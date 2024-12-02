88418d6531bcfd0505838301c7df74275b63fd5c
Priyanka <pri.ppk@gmail.com>
2024-12-02 14:45:45 -0800

    Add index.html, css.html files. Implement wave 2 functionality for addOneTemp and reduceOneTemp with click.

---
+++ src/index.js
@@ -0,0 +1,20 @@
+let temperature = 88;
+
+const tempDisplay = document.getElementById('tempValue');
+tempDisplay.innerText = temperature;
+
+const addOneTemp = (e) => {
+    console.log(temperature)
+    temperature += 1;
+    tempDisplay.innerText = temperature;
+}; 
+const increaseTemp = document.getElementById('increaseTempControl');
+increaseTemp.addEventListener('click', addOneTemp);
+
+const reduceOneTemp = () => {
+    console.log(temperature)
+    temperature -= 1;
+    tempDisplay.innerText = temperature;
+};
+const decreaseTemp = document.getElementById('decreaseTempControl');
+decreaseTemp.addEventListener('click', reduceOneTemp);
