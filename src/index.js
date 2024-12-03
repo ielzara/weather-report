b0cc19e3cc248e5fb680da424cd55d82d38e3290
Priyanka <pri.ppk@gmail.com>
2024-12-03 10:12:16 -0800

    Add wave 3 implementation.

--- src/index.js
+++ src/index.js
@@ -2,30 +2,32 @@ const tempDisplay = document.getElementById('tempValue');
 const increaseTemp = document.getElementById('increaseTempControl');
 const decreaseTemp = document.getElementById('decreaseTempControl');
 const landscape = document.getElementById('landscape');
+const cityNameDisplay = document.getElementById('headerCityName');
+const cityNameInput = document.getElementById('cityNameInput')
 
 let temperature = 88;
 tempDisplay.innerText = temperature;
 
 // Color of the temperature value number
 const updateTempColor = () => {
-  tempDisplay.classList.remove('red', 'orange', 'yellow', 'green', 'teal');
+tempDisplay.classList.remove('red', 'orange', 'yellow', 'green', 'teal');
 
-  if (temperature >= 80) {
+if (temperature >= 80) {
     tempDisplay.classList.add('red');
-  } else if (temperature >= 70) {
+} else if (temperature >= 70) {
     tempDisplay.classList.add('orange');
-  } else if (temperature >= 60) {
+} else if (temperature >= 60) {
     tempDisplay.classList.add('yellow');
-  } else if (temperature >= 50) {
+} else if (temperature >= 50) {
     tempDisplay.classList.add('green');
-  } else {
+} else {
     tempDisplay.classList.add('teal');
-  }
+}
 };
 
 // Landscape emojis
 const updateLandscape = ()=>  {
-  let landscapeDisplay;
+let landscapeDisplay;
 
     if (temperature >= 80) {
         landscapeDisplay = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
@@ -62,5 +64,12 @@ const reduceOneTemp = () => {
 
 increaseTemp.addEventListener('click', addOneTemp);
 decreaseTemp.addEventListener('click', reduceOneTemp);
+
+const updateCityName = () => {
+    const cityText = cityNameInput.value;
+    cityNameDisplay.innerText = cityText;
+};
+
+cityNameInput.addEventListener('input', updateCityName);
 
 updateDisplay();
