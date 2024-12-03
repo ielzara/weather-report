2133d3e6ce70a7e209d42edca5df49aa37992d6d
Priyanka <pri.ppk@gmail.com>
2024-12-03 09:55:47 -0800

    Wave 2 implementation

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const addOneTemp = () => {
     console.log(temperature)
     temperature += 1;
     tempDisplay.innerText = temperature;
+    updateColor();
+    updateLandscape();
 }; 
 const increaseTemp = document.getElementById('increaseTempControl');
 increaseTemp.addEventListener('click', addOneTemp);
@@ -15,6 +17,33 @@ const reduceOneTemp = () => {
     console.log(temperature)
     temperature -= 1;
     tempDisplay.innerText = temperature;
+    updateColor();
+    updateLandscape();
 };
 const decreaseTemp = document.getElementById('decreaseTempControl');
 decreaseTemp.addEventListener('click', reduceOneTemp);
+
+const updateColor = () => {
+    if (temperature >= 80) {
+        tempDisplay.style.color = 'red';
+    } else if (temperature >= 70) {
+        tempDisplay.style.color = 'orange';
+    } else if (temperature >= 60) {
+        tempDisplay.style.color = 'yellow';
+    } else if (temperature >= 50) {
+        tempDisplay.style.color = 'green';
+    } else {
+        tempDisplay.style.color = 'teal';
+    }
+};
+
+const landscapeDisplay = document.getElementById('landscape');
+
+const updateLandscape = () => {
+    if (temperature >= 80) {
+        landscapeDisplay.innerText = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂'
+    }
+    } else if (temperature >= 70) {
+        landscapeDisplay.innerText = 
+    }
+};
